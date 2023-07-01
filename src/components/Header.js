13d1da1206a87b3b6f6059cd1a6940241649c67e
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-07-01 22:21:26 +0530

    second

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className='left'>
-       <h3>Shopping Cart</h3>
+    <div className="header">
+      <div className="left">
+        <h3>Shopping Cart</h3>
       </div>
       <nav>
-         <NavLink to='/' className='ancher'>Home Page</NavLink>
-         <NavLink to='/cart' className='ancher'>Cart Page</NavLink>
+        <NavLink to="/f4-contest3" className="ancher">
+          Home Page
+        </NavLink>
+        <NavLink to="/cart" className="ancher">
+          Cart Page
+        </NavLink>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
